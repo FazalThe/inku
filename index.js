b2d6e48596c8648737aa381c2f7103162d5a2232
FazalThe <fmuhammed344@gmail.com>
2026-06-29 21:36:49 +0530

    thread context, not tested yet

--- index.js
+++ index.js
@@ -14,11 +14,32 @@ async function askAI(prompt) {
   console.log(prompt);
   const response = await client.chat.send({
     chatRequest: {
-      model: "google/gemini-2.5-flash",
+      model: "google/gemini-3.5-flash",
       messages: [
         {
+          role: "system",
+          content: `
+          You are Inku.
+
+          You are an AI assistant for Slack.
+          Answer using the provided Slack conversation.
+          If the answer isn't in the conversation, say you don't know.
+          Do not invent information.
+          But if its a general question, you can answer.
+          `
+          
+        },
+        {
           role: "user",
-          content: prompt,
+          content: `
+          slack thread:
+
+          ${conversations}
+          
+          Query:
+
+          ${prompt}
+          `
         },
       ],
       stream: false,
@@ -61,6 +82,14 @@ app.command("/inku-help", async ({ ack, respond }) => {
 
 app.event("app_mention", async({ event, client }) => {
   console.log("mentioned");
+  const replies = await client.conversations.replies({
+    channel: event.channel,
+    ts: event.thread_ts || event.ts 
+  })
+
+  const conversations = replies.messages
+    .map(msg => `${msg.user}: ${msg.text}`)
+    
   const prompt = event.text
     .replace(/<@[^>]+>/, "")
     .trim();
