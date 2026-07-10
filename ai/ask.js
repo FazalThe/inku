14ff12082252fbf5146e92973493d57a04a041c6
FazalThe <fmuhammed344@gmail.com>
2026-07-10 12:02:35 +0530

    some ai tools looping

--- ai/ask.js
+++ ai/ask.js
@@ -2,43 +2,54 @@ import client from "./client.js";
 
 import { tools } from "./tools.js"
 
+
+const messages = [
+  {
+    role: "system",
+    content: `
+    You are Inku.
+    You are an AI assistant for Slack.
+    Answer using the provided Slack conversation.
+    In the conversation U0A0LCKQH4J is yourself.
+    If the answer isn't in the conversation, say you don't know.
+    Do not invent information.
+    But if its a general question, you can answer.
+    `
+    
+  },
+  {
+    role: "user",
+    content: `
+    slack thread:
+    ${conversations}
+    
+    Query:
+    ${prompt}
+    `
+  }
+];
+
+
 export async function askAI(prompt, conversations) {
   console.log(prompt);
   const response = await client.chat.send({
     chatRequest: {
       model: "google/gemini-3.5-flash",
-      messages: [
-        {
-          role: "system",
-          content: `
-          You are Inku.
-
-          You are an AI assistant for Slack.
-          Answer using the provided Slack conversation.
-          In the conversation U0A0LCKQH4J is yourself.
-          If the answer isn't in the conversation, say you don't know.
-          Do not invent information.
-          But if its a general question, you can answer.
-          `
-          
-        },
-        {
-          role: "user",
-          content: `
-          slack thread:
-
-          ${conversations}
-          
-          Query:
-
-          ${prompt}
-          `
-        },
-      ],
+      messages,
       tools,
       stream: false,
     },
   });
 
-  return response;
+  messages.push(response.choices[0].message);
+
+  const role = response.choices[0].message.role;
+
+  if (role === "user"){
+    return response;
+  }
+
+  if (role === "assistant"){
+
+  }
 }
