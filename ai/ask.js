7407418b670ce5c6b655019a8bd60c888196b6ad
FazalThe <fmuhammed344@gmail.com>
2026-07-11 17:08:21 +0530

    bot is running now

--- ai/ask.js
+++ ai/ask.js
@@ -4,35 +4,36 @@ import { tools } from "./tools.js";
 import toolMap from "./functions.js";
 
 
-const messages = [
-  {
-    role: "system",
-    content: `
-    You are Inku.
-    You are an AI assistant for Slack.
-    Answer using the provided Slack conversation.
-    In the conversation U0A0LCKQH4J is yourself.
-    If the answer isn't in the conversation, say you don't know.
-    Do not invent information.
-    But if its a general question, you can answer.
-    `
-    
-  },
-  {
-    role: "user",
-    content: `
-    slack thread:
-    ${conversations}
-    
-    Query:
-    ${prompt}
-    `
-  }
-];
 
 
 export async function askAI(prompt, conversations) {
-  console.log(prompt);
+
+  const messages = [
+    {
+      role: "system",
+      content: `
+      You are Inku.
+      You are an AI assistant for Slack.
+      Answer using the provided Slack conversation.
+      In the conversation U0A0LCKQH4J is yourself.
+      If the answer isn't in the conversation, say you don't know.
+      Do not invent information.
+      But if its a general question, you can answer.
+      `
+      
+    },
+    {
+      role: "user",
+      content: `
+      slack thread:
+      ${conversations}
+      
+      Query:
+      ${prompt}
+      `
+    }
+  ];
+
   const response = await client.chat.send({
     chatRequest: {
       model: "google/gemini-3.5-flash",
@@ -49,7 +50,7 @@ export async function askAI(prompt, conversations) {
   for (const toolCall of answer.toolCalls){
 
     const toolName = toolCall.function.name;
-    const arguments = toolCall.function.arguments;
+    const terms = toolCall.function.arguments;
 
     const toolResponse = await toolMap[toolName](arguments);
   }
